94f9a9bc798b7e834631432ccf069899372380af
Chao Zhang <maczhang.rock@gmail.com>
2020-06-01 00:45:15 -0700

    first push

--- download.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-
-import os
-
-for i in range(1,5):
-
-	os.system("wget https://www2.census.gov/programs-surveys/demo/tables/metro-micro/2015/commuting-flows-2015/table{table_number}.xlsx -O table{table_number}.xlsx".format(table_number=str(i)))
