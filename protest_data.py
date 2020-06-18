96e832d53f6ca75edb317d8744706f21e6a7e3cf
Chao Zhang <maczhang.rock@gmail.com>
2020-06-17 20:53:12 -0700

    local sync

---
+++ protest_data.py
@@ -0,0 +1,21 @@
+### scripts to download, process, and onboarding the protest data
+
+import pandas as pd
+import numpy as np
+
+
+
+def download_protest():
+
+
+
+	return 
+
+def process_protest():
+
+
+	return 
+
+def check_updates():
+
+	return
