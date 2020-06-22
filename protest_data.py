ab063d43b30ef6c49c9c74bdf912460a1a0a4a4f
Chao <maczhang.rock@gmail.com>
2020-06-21 21:55:57 -0700

    finalizing protest data

--- protest_data.py
+++ protest_data.py
@@ -5,7 +5,23 @@ import numpy as np
 
 
 
-def download_protest():
+def download():
+	# download protest data from google spreadsheet
+
+	may_tally_url =
+	may_gf_url = 
+	june_tally_url = 
+	june_gf_url = 
+
+	may_tally = pd.read_csv(may_tally_url)
+
+	may_gf = pd.read_csv(may_gf_url)
+	
+	june_tally = pd.read_csv(june_tally_url)
+	
+	june_gf = pd.read_csv(june_gf_url)
+
+
 
 
 
